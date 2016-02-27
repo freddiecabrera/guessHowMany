66aded5e3276ea704df971fac374a54aa757917c
freddiecabrera <freddiecabrera18@yahoo.com>
2016-02-26 21:43:19 -0800

    added random amount of gators

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+var gator = 'http://i.imgur.com/Rg6G77m.png?1';
+function randomNumberOfGators() {
+  //var gators = $('.cage').html('<img src="' + gator + '" width="200" height="200">');
+  var randomNumber = Math.floor((Math.random() * 9) + 1);
+  for(var i = 0; i < randomNumber; i++){
+    $('.cage').append('<img src="' + gator + '" width="200" height="200">');
+    console.log(randomNumber[i]);
+  }
+}
 
+$(function() {
+  randomNumberOfGators();
+
+});
